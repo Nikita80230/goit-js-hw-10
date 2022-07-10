01263e48e9eb9dac4e11518f64f0f6cbcf6604b6
Nikita_Beliy <beliynikita1995@gmail.com>
2022-07-10 21:25:18 +0200

    add promice

---
+++ src/fetchCountries.js
@@ -0,0 +1,6 @@
+export function fetchCountries(countryName) {
+    return fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+        .then(response => response.json());
+
+}
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+import debounce from 'lodash.debounce';
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 600;
+
+const refs = {
+    input: document.querySelector('#search-box'),
+    list: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+};
+
+
+
+
+//  https://restcountries.com/v2/all?fields=name,capital,currencies
+
+const debouncedOnInput = debounce(onInput, DEBOUNCE_DELAY)
+
+refs.input.addEventListener('input', debouncedOnInput);
+
+function onInput(e) {
+    e.preventDefault();
+    console.log(refs.input.value);
+    fetchCountries(refs.input.value).then(country => {
+    console.log(country);
+});
+};
+
+// нужно узнать как получать только нужные свойства
